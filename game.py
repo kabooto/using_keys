fa5d6894c529e378f99826d863eda7518c9324b5
Alex Sher <sher6259@mail.ru>
2022-12-12 20:33:29 +0600

    made a pygame inicialization

--- game.py
+++ game.py
@@ -1,1 +1,26 @@
+import pygame as pg
 import pygame.draw as dr
+
+pg.init()
+
+
+def esc_exit():
+    global finished, event
+    if event.type == pg.KEYDOWN:
+        if event.key == pg.K_ESCAPE:
+            finished = True
+            pg.quit()
+
+
+FPS = 1
+size = [500, 500]
+screen = pg.display.set_mode(size)
+
+finished = False
+
+pg.display.update()
+clock = pg.time.Clock()
+while not finished:
+    clock.tick(FPS)
+    for event in pg.event.get():
+        esc_exit()
