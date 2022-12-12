dcde330dfa7a921f6347eaf4381c4821cfaebf8b
Alex Sher <sher6259@mail.ru>
2022-12-12 21:43:30 +0600

    made little refractoring

--- game.py
+++ game.py
@@ -5,27 +5,24 @@ pg.init()
 
 
 def esc_exit():
+    """
+    exit game by escape pressing
+    :return: None
+    """
     global finished, event
     if event.type == pg.KEYDOWN:
         if event.key == pg.K_ESCAPE:
             finished = True
-            pg.quit()
+            exit()
 
 
-FPS = 10
-size = [500, 500]
-screen = pg.display.set_mode(size)
-
-finished = False
-
-x, y = 250, 250
-pers_speed = 5
-pg.display.update()
-clock = pg.time.Clock()
-while not finished:
-    clock.tick(FPS)
-    for event in pg.event.get():
-        esc_exit()
+def player():
+    """
+    Makes a player movement object
+    :return: None
+    """
+    global x, y
+    pers_speed = 5
 
     keys = pg.key.get_pressed()
 
@@ -40,4 +37,20 @@ while not finished:
 
     screen.fill('Black')
     dr.circle(screen, 'Red', (x, y), 10)
+
+
+FPS = 10
+size = [500, 500]
+screen = pg.display.set_mode(size)
+
+x, y = 250, 250
+finished = False
+
+pg.display.update()
+clock = pg.time.Clock()
+while not finished:
+    clock.tick(FPS)
+    for event in pg.event.get():
+        esc_exit()
+    player()
     pg.display.update()
