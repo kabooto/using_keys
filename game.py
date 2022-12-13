543f2be56810a95f469dddc65c9e9ad566c17d28
Alex Sher <sher6259@mail.ru>
2022-12-13 21:08:14 +0600

    made a new ball which goes randomly

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import pygame.draw as dr
+from random import randint as rint
 
 pg.init()
 
@@ -39,11 +40,21 @@ def player():
     dr.circle(screen, 'Red', (x, y), 10)
 
 
+def new_ball():
+    global x1, y1
+    x1 += rint(0, 6)
+    x1 -= rint(0, 6)
+    y1 += rint(0, 6)
+    y1 -= rint(0, 6)
+    dr.circle(screen, 'White', (x1, y1), 10)
+
+
 FPS = 10
 size = [500, 500]
 screen = pg.display.set_mode(size)
 
 x, y = 250, 250
+x1, y1 = 250, 250
 finished = False
 
 pg.display.update()
@@ -53,4 +64,5 @@ while not finished:
     for event in pg.event.get():
         esc_exit()
     player()
+    new_ball()
     pg.display.update()
