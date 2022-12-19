1bc01e530898d2c1facd39d04473d16883ee21f2
Alex Sher <sher6259@mail.ru>
2022-12-19 20:15:00 +0600

    I did refactoring by classes and objects, became pretty simple to understand and add new features

--- game.py
+++ game.py
@@ -5,12 +5,11 @@ from random import randint
 pg.init()
 
 
-def esc_exit():
+def esc_exit(finished, event):
     """
     exit game by escape pressing
     :return: None
     """
-    global finished, event
     if event.type == pg.KEYDOWN:
         if event.key == pg.K_ESCAPE:
             finished = True
@@ -47,10 +46,10 @@ class NPC:
         self.x, self.y = 250, 5
         self.size = 10
         dr.circle(screen, 'White', (self.x, self.y), self.size)
-
-    def moves(self, screen):
         self.speed_x = 0
         self.speed_y = 5
+
+    def moves(self, screen):
         if self.x > 495:
             self.speed_x = -5
             self.speed_y = randint(-5, 6)
@@ -61,7 +60,7 @@ class NPC:
             self.speed_y = -5
             self.speed_x = randint(-5, 6)
         if self.y < 5:
-            self.speed_x = 5
+            self.speed_y = 5
             self.speed_x = randint(-5, 6)
         self.x += self.speed_x
         self.y += self.speed_y
@@ -69,39 +68,13 @@ class NPC:
 
     def collision(self):
         pass
-'''def ball_npc():
-    """
-    Makes not manage ball movements
-    :return: None
-    """
-    global x1, y1, speed_x, speed_y
-    if x1 > 495:  # right wall
-        speed_x = -5
-        speed_y = randint(-5, 6)
-    if x1 < 5:  # left wall
-        speed_x = 5
-        speed_y = randint(-5, 6)
-    if y1 > 495:  # Screen top
-        speed_y = -5
-        speed_x = randint(-5, 6)
-    if y1 < 5:  # Screen bottom
-        speed_y = 5
-        speed_x = randint(-5, 6)
-    x1 += speed_x
-    y1 += speed_y
-    dr.circle(screen, 'White', (x1, y1), 10)'''
 
 
 def main():
-    global event, x1, y1, x2, y2, speed_x, speed_y, speed_x2, speed_y2
     FPS = 20
     size = [500, 500]
     screen = pg.display.set_mode(size)
 
-    x1, y1 = 300, 250
-    x2, y2 = 0, 0
-    speed_x, speed_y = 5, 0
-    speed_x2, speed_y2 = 5, 0
     finished = False
 
     pg.display.update()
@@ -111,8 +84,7 @@ def main():
     while not finished:
         clock.tick(FPS)
         for event in pg.event.get():
-            esc_exit()
-        #ball_player()
+            esc_exit(finished, event)
         player.moves(screen)
         npc.moves(screen)
         pg.display.update()
